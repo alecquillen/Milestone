34dba99458c9f5fb9ffdc1bd01297b53fb56d363
alecquillen <aquille2@students.kennesaw.edu>
2024-06-29 21:30:15 -0400

    m22

--- books.js
+++ books.js
@@ -2,57 +2,43 @@ $(document).ready(function () {
     const API_KEY = 'YOUR_GOOGLE_BOOKS_API_KEY';
     const RESULTS_PER_PAGE = 10;
 
-    function showHomeSection() {
-        $('#home-section').show();
-        $('#book-details-container').hide();
-        $('#bookshelf-section').hide();
-    }
-
-    function showBookDetailsSection() {
-        $('#home-section').hide();
-        $('#book-details-container').show();
-        $('#bookshelf-section').hide();
-    }
-
-    function showBookshelfSection() {
-        $('#home-section').hide();
-        $('#book-details-container').hide();
-        $('#bookshelf-section').show();
-    }
-
-    $('#home-menu').click(showHomeSection);
-    $('#bookshelf-menu').click(showBookshelfSection);
-
-    $('#search-button').click(function () {
-        const searchTerm = $('#searchInput').val();
-        const url = `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=${API_KEY}&maxResults=${RESULTS_PER_PAGE}`;
-
-        $.getJSON(url, function (data) {
-            displaySearchResults(data);
+    // Home/Book Search Page
+    if (window.location.pathname.includes('index.html')) {
+        $('#search-button').click(function () {
+            const searchTerm = $('#searchInput').val();
+            const url = `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=${API_KEY}&maxResults=${RESULTS_PER_PAGE}`;
+    
+            $.getJSON(url, function (data) {
+                displaySearchResults(data);
+            });
         });
-    });
+    
+        function displaySearchResults(data) {
+            let searchResultsContainer = $('#searchResults');
+            searchResultsContainer.empty();
+    
+            if (data.items) {
+                data.items.forEach(book => {
+                    let volumeInfo = book.volumeInfo;
+                    let bookHtml = `
+                        <div class="book">
+                            <h4><a href="book-details.html?id=${book.id}" class="book-link">${volumeInfo.title}</a></h4>
+                            <p>By: ${volumeInfo.authors.join(', ')}</p>
+                            <img src="${volumeInfo.imageLinks ? volumeInfo.imageLinks.thumbnail : ''}" alt="Book Cover">
+                        </div>
+                    `;
+                    searchResultsContainer.append(bookHtml);
+                });
+            }
+        }
+    }
 
-    function displaySearchResults(data) {
-        let searchResultsContainer = $('#searchResults');
-        searchResultsContainer.empty();
-
-        if (data.items) {
-            data.items.forEach(book => {
-                let volumeInfo = book.volumeInfo;
-                let bookHtml = `
-                    <div class="book">
-                        <h4><a href="#" class="book-link" data-id="${book.id}">${volumeInfo.title}</a></h4>
-                        <p>By: ${volumeInfo.authors.join(', ')}</p>
-                        <img src="${volumeInfo.imageLinks ? volumeInfo.imageLinks.thumbnail : ''}" alt="Book Cover">
-                    </div>
-                `;
-                searchResultsContainer.append(bookHtml);
-            });
-
-            $('.book-link').click(function () {
-                const bookId = $(this).data('id');
-                fetchBookDetails(bookId);
-            });
+    // Book Details Page
+    if (window.location.pathname.includes('book-details.html')) {
+        const urlParams = new URLSearchParams(window.location.search);
+        const bookId = urlParams.get('id');
+        if (bookId) {
+            fetchBookDetails(bookId);
         }
     }
 
@@ -61,7 +47,6 @@ $(document).ready(function () {
 
         $.getJSON(url, function (data) {
             displayBookDetails(data);
-            showBookDetailsSection();
         });
     }
 
@@ -85,7 +70,11 @@ $(document).ready(function () {
         bookDetailsContainer.html(bookHtml);
     }
 
-    // Fetch and display books from public bookshelf
+    // My Bookshelf Page
+    if (window.location.pathname.includes('bookshelf.html')) {
+        fetchBookshelf();
+    }
+
     function fetchBookshelf() {
         const bookshelfId = 'YOUR_PUBLIC_BOOKSHELF_ID';
         const url = `https://www.googleapis.com/books/v1/users/YOUR_USER_ID/bookshelves/${bookshelfId}/volumes?key=${API_KEY}`;
@@ -104,21 +93,13 @@ $(document).ready(function () {
                 let volumeInfo = book.volumeInfo;
                 let bookHtml = `
                     <div class="book">
-                        <h4><a href="#" class="book-link" data-id="${book.id}">${volumeInfo.title}</a></h4>
+                        <h4><a href="book-details.html?id=${book.id}" class="book-link">${volumeInfo.title}</a></h4>
                         <p>By: ${volumeInfo.authors.join(', ')}</p>
                         <img src="${volumeInfo.imageLinks ? volumeInfo.imageLinks.thumbnail : ''}" alt="Book Cover">
                     </div>
                 `;
                 bookshelfContainer.append(bookHtml);
             });
-
-            $('.book-link').click(function () {
-                const bookId = $(this).data('id');
-                fetchBookDetails(bookId);
-            });
         }
     }
-
-    // Initial fetch of the bookshelf
-    fetchBookshelf();
 });
