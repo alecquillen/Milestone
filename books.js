c2ffd3c71dca8cffe47eba7c68a1e328ddfffbe0
alecquillen <aquille2@students.kennesaw.edu>
2024-07-02 19:45:44 -0400

    search

--- books.js
+++ books.js
@@ -1,7 +1,6 @@
-// books.js - Updated Functions
-
-const API_KEY = 'AIzaSyBMtgM5pmAm79-KkyDbpVo_Jq-dKCTKN0I'; 
+const API_KEY = 'AIzaSyBMtgM5pmAm79-KkyDbpVo_Jq-dKCTKN0I';
 const RESULTS_PER_PAGE = 10;
+const bookshelf = [];  // Local bookshelf array to hold book IDs
 
 // Home/Book Search Page
 function searchBooks() {
@@ -29,6 +28,7 @@ function displaySearchResults(data) {
                     <h4><a href="book-details.html?id=${book.id}" class="book-link">${volumeInfo.title}</a></h4>
                     <p>By: ${volumeInfo.authors ? volumeInfo.authors.join(', ') : 'Unknown Author'}</p>
                     <img src="${volumeInfo.imageLinks ? volumeInfo.imageLinks.thumbnail : ''}" alt="Book Cover">
+                    <button onclick="addToBookshelf('${book.id}')">Add to Bookshelf</button>
                 </div>
             `;
             searchResultsContainer.innerHTML += bookHtml;
@@ -59,40 +59,44 @@ function fetchPage(page, searchTerm) {
         .catch(error => console.error('Error fetching data:', error));
 }
 
-// My Bookshelf Page
-function loadBookshelf() {
-    const bookshelfId = '1001'; 
-    const url = `https://www.googleapis.com/books/v1/users/117729639710872190748/bookshelves/${bookshelfId}/volumes?key=${API_KEY}`;
-
-    fetch(url)
-        .then(response => response.json())
-        .then(data => displayBookshelf(data))
-        .catch(error => console.error('Error fetching data:', error));
+function addToBookshelf(bookId) {
+    if (!bookshelf.includes(bookId)) {
+        bookshelf.push(bookId);
+        alert('Book added to bookshelf');
+    } else {
+        alert('Book already in bookshelf');
+    }
 }
 
-function displayBookshelf(data) {
+// My Bookshelf Page
+function loadBookshelf() {
     const bookshelfContainer = document.getElementById('bookshelf-container');
     bookshelfContainer.innerHTML = '';
 
-    if (data.items) {
-        data.items.forEach(book => {
-            const volumeInfo = book.volumeInfo;
-            const bookHtml = `
-                <div class="book">
-                    <h4><a href="book-details.html?id=${book.id}" class="book-link">${volumeInfo.title}</a></h4>
-                    <p>By: ${volumeInfo.authors ? volumeInfo.authors.join(', ') : 'Unknown Author'}</p>
-                    <img src="${volumeInfo.imageLinks ? volumeInfo.imageLinks.thumbnail : ''}" alt="Book Cover">
-                </div>
-            `;
-            bookshelfContainer.innerHTML += bookHtml;
-        });
-    }
+    bookshelf.forEach(bookId => {
+        const url = `https://www.googleapis.com/books/v1/volumes/${bookId}?key=${API_KEY}`;
+        
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                const volumeInfo = data.volumeInfo;
+                const bookHtml = `
+                    <div class="book">
+                        <h4><a href="book-details.html?id=${book.id}" class="book-link">${volumeInfo.title}</a></h4>
+                        <p>By: ${volumeInfo.authors ? volumeInfo.authors.join(', ') : 'Unknown Author'}</p>
+                        <img src="${volumeInfo.imageLinks ? volumeInfo.imageLinks.thumbnail : ''}" alt="Book Cover">
+                    </div>
+                `;
+                bookshelfContainer.innerHTML += bookHtml;
+            })
+            .catch(error => console.error('Error fetching data:', error));
+    });
 }
 
 // Load functions based on the current page
 document.addEventListener('DOMContentLoaded', function() {
-    if (window.location.pathname.includes('index.html')) {
-        document.getElementById('search-button').onclick = searchBooks;
+    if (window.location.pathname.includes('homepage.html')) {
+        document.getElementById('searchButton').onclick = searchBooks;
     } else if (window.location.pathname.includes('bookshelf.html')) {
         loadBookshelf();
     }
